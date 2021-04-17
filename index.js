cfd9859090bfba2c0647d647815405a3a18f33bc
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-04-17 23:08:10 +0530

    Faqs page made

---
+++ index.js
@@ -0,0 +1,34 @@
+$(".queone").click(function(){
+  console.log("clicked");
+  $(".ansone").slideToggle();
+  $(".queone").toggleClass("bold");
+  $(".downarrowone").toggleClass("rotate");
+});
+
+$(".quetwo").click(function(){
+  console.log("clicked");
+  $(".anstwo").slideToggle();
+  $(".quetwo").toggleClass("bold");
+  $(".downarrowtwo").toggleClass("rotate");
+});
+
+$(".quethree").click(function(){
+  console.log("clicked");
+  $(".ansthree").slideToggle();
+  $(".quethree").toggleClass("bold");
+  $(".downarrowthree").toggleClass("rotate");
+});
+
+$(".quefour").click(function(){
+  console.log("clicked");
+  $(".ansfour").slideToggle();
+  $(".quefour").toggleClass("bold");
+  $(".downarrowfour").toggleClass("rotate");
+});
+
+$(".quefive").click(function(){
+  console.log("clicked");
+  $(".ansfive").slideToggle();
+  $(".quefive").toggleClass("bold");
+  $(".downarrowfive").toggleClass("rotate");
+});
